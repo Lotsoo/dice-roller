b0191dda87aaadd0597cd5a6e51563bcd3bf3b84
lotsoo <braindeathrama@gmail.com>
2024-08-17 21:32:24 +0700

    feat: added button to roll dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,6 +42,9 @@ fun DiceRollerApp(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize()
     ) {
         Dice()
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Roll")
+        }
     }
 }
 
